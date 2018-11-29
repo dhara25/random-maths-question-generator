c9fa7d5db4f0ae5c6e946613c6949704ddcdef46
dhara25 <45458533+dhara25@users.noreply.github.com>
2018-11-29 19:34:12 +0530

    types.h

---
+++ types.h
@@ -0,0 +1,24 @@
+ifndef TYPES_H
+#define TYPES_H
+
+void correct();
+void GrandnumB();
+void GrandnumD();
+void MultiplyB();
+void AddB();
+void SubtractB();
+void DivideB();
+void MultiplyD();
+void AddD();
+void SubtractD();
+void DivideD();
+void qtypef();
+extern int qtype1;
+extern double qnum1;
+extern double qnum2;
+extern double qnuma1;
+extern double qnuma2;
+extern char ans1;
+extern int count;
+
+#endif
